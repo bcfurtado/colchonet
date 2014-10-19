48a4cfdf3221dd8817367704e522e487eae25967
Bruno Furtado <brunofurtadoc@gmail.com>
2014-10-18 23:46:59 -0300

    added user and room feature

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new    
+  end
+
+  def create
+    @user = User.create(create_params)
+    if @user.save
+      redirect_to @user, :notice => 'Cadastro criado com sucesso!'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(create_params)
+      redirect_to @user, :notice => 'Cadastro atualizado com sucesso!'
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+
+    def create_params
+      params.require(:user).permit(:email, :full_name, :bio, :location,
+        :password, :password_confirmation)
+    end
+
+end
